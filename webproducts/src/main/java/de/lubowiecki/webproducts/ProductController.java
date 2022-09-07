7d3c8d339e8cd85aa6be8b4290f0e1117905d126
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-09-07 13:42:00 +0200

    Update

--- webproducts/src/main/java/de/lubowiecki/webproducts/ProductController.java
+++ webproducts/src/main/java/de/lubowiecki/webproducts/ProductController.java
@@ -3,6 +3,8 @@ package de.lubowiecki.webproducts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,12 +22,26 @@ public class ProductController {
 	
 	@RequestMapping("/new")
 	public String productForm(Model model) {
+		model.addAttribute("product", new Product());
 		return "form";
 	}
 	
 	@RequestMapping("/save")
 	public String save(Product p) {
 		repo.save(p);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model model) {
+		Product p = repo.findById(id).orElse(new Product());
+		model.addAttribute("product", p);
+		return "form";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable int id) {
+		repo.deleteById(id);
 		return "redirect:/";
 	}
 }
